6d8a270f99ed0bb137e7853cfa2357930679feab
MauroDS <mauro.diaz.santilli@gmail.com>
2023-06-03 14:00:56 -0300

    Añadiendo icono a titulo

--- src/components/Titulo.jsx
+++ src/components/Titulo.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faNewspaper } from '@fortawesome/free-solid-svg-icons';
 
 function Titulo(){
     return(
         <section className="text-center">
-        <h1>Portal noticias</h1>
+        <h1><FontAwesomeIcon icon={faNewspaper} flip className='me-2' />Portal noticias</h1>
         <hr />
         </section>
     )
